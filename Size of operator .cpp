f1d6d3928f01a2731529f26c2afee076f03d2eb2
Himalmevada <63632500+Himalmevada@users.noreply.github.com>
2020-05-03 20:42:11 +0530

    Size of operator
    here i made one program which shows how many bytes your particular datatype allocates in the memory in C++.

---
+++ Size of operator .cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    float f;
+    char c;
+    double d;
+
+    cout<<"Size of char   : "<<sizeof(c)<<" byte"<<endl;
+    cout<<"Size of int    : "<<sizeof(i)<<" byte"<<endl;
+    cout<<"Size of float  : "<<sizeof(f)<<" byte"<<endl;
+    cout<<"Size of double : "<<sizeof(d)<<" byte"<<endl;
+    return 0;
+}
